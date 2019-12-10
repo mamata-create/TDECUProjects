81a4a34d7d1c0ad00e462af548c8fe4021588cb6
Divya <divya.vagalaboina@eaglecrk.com>
2019-12-10 14:38:52 -0500

    New script added.

---
+++ InstantOpen/src/test/java/ApplicantInfo/C23962_NonMemberUserShouldEnterAllMandatoryDetailsInContactInformationSection.java
@@ -0,0 +1,248 @@
+package ApplicantInfo;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.FrameworkComponents.BaseClass;
+import com.FrameworkComponents.ExtentManager;
+import com.FrameworkComponents.GenericKeywords;
+import com.FrameworkComponents.ObjectRepository;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.github.javafaker.Faker;
+
+public class C23962_NonMemberUserShouldEnterAllMandatoryDetailsInContactInformationSection extends GenericKeywords {
+
+	ExtentReports extent;
+	ExtentTest test;
+	/*
+	 * Verify that Non member user Should enter all mandatory field details in contact information section
+	 */
+
+	@BeforeTest
+	public void setUp() throws InterruptedException, MessagingException, IOException {
+		
+		extent = ExtentManager.GetExtent();
+		test = extent.createTest(this.getClass().getSimpleName());
+		BaseClass iDriver = new BaseClass();
+		iDriver.myTestCaseName = this.getClass().getSimpleName();
+		executeDriverScript();
+		test.log(Status.INFO, "Application url navigated");
+		
+	}	
+	
+	@Test
+	public void C23962_NonMemberUserShouldEnterAllMandatoryDetailsInContactInformationSection() throws InterruptedException, MessagingException, IOException
+	{
+		
+		if(continuetestcase==true)
+		{
+			sheetName = "Data";
+			int totalRowCount = excl.getRowCount(sheetName);
+			for(startIter=1;startIter<=totalRowCount;startIter++) //It is mandatory to have this for loop in every test case
+			 {	
+				if(this.getClass().getSimpleName().equals(excl.getCellData("Data", 0, startIter)))
+				 {
+					Faker fk=new Faker();
+					String num=getRandom();
+					String fname=fk.name().firstName();
+					
+					String lname=fk.name().lastName();
+					
+					String strtaddress=excl.getCellData(sheetName, 5, startIter);
+					String zipcode=excl.getCellData(sheetName, 6, startIter);
+					String ssn=excl.getCellData(sheetName, 7, startIter);
+					ssn=ssn+num;
+					String dob=excl.getCellData(sheetName, 8, startIter);
+					dob=dob+num;
+					String idtyp=excl.getCellData(sheetName, 9, startIter);
+					String idnumber=excl.getCellData(sheetName, 10, startIter);
+					String issue_dt=excl.getCellData(sheetName, 11, startIter);
+					String expr_dt=excl.getCellData(sheetName, 12, startIter);
+					String mothername=excl.getCellData(sheetName, 13, startIter);
+					String crrnt_emplyr=excl.getCellData(sheetName, 14, startIter);
+					String occupation=excl.getCellData(sheetName, 15, startIter);
+					String phonenmbr=excl.getCellData(sheetName, 16, startIter);
+					String phonetyp=excl.getCellData(sheetName, 17, startIter);
+					String primary_email=excl.getCellData(sheetName, 18, startIter);
+					String hear_opt=excl.getCellData(sheetName, 19, startIter);
+					String bottomMsg = excl.getCellData(sheetName, 26, startIter);
+					String informationHeader=excl.getCellData(sheetName, 23, startIter);
+					String informationContent = excl.getCellData(sheetName, 24, startIter);
+					String checkingAccountOptionHeader =  excl.getCellData(sheetName, 25, startIter);
+					
+					verifyElementPresent(ObjectRepository.app_ttl);
+					test.log(Status.INFO, "Instant Open Title appearing");
+					
+					scrollToElement(ObjectRepository.nonmmbrstrt_btn);
+					getElement(ObjectRepository.nonmmbrstrt_btn).click();
+					test.log(Status.INFO, "Non Member Start here button clicked");
+					
+					Thread.sleep(3000);
+					
+					verifyText(ObjectRepository.inforMsgHeader,informationHeader);
+					verifyText(ObjectRepository.inforMsgContent,informationContent);
+					verifyText(ObjectRepository.checkingAccountOptionHeader,checkingAccountOptionHeader);
+					
+					scrollToElement(ObjectRepository.nochkacnt_rdbtn);
+					getElement(ObjectRepository.nochkacnt_rdbtn).click();
+					test.log(Status.INFO, "Dont want checking account radio button clicked");
+					
+					
+					getElement(ObjectRepository.othrprdctno_rdbtn).click();
+					test.log(Status.INFO, "Other products no radio button clicked");
+					
+					getElement(ObjectRepository.continue_btn).click();
+					test.log(Status.INFO, "Continue button clicked");
+					
+					getElement(ObjectRepository.fname_txt).sendKeys(fname);
+					test.log(Status.INFO, "First name entered");
+					
+//					getElement(ObjectRepository.mname_txt).sendKeys("");
+//					test.log(Status.INFO, "Middle name entered");
+//					
+					getElement(ObjectRepository.lname_txt).sendKeys(lname);
+					test.log(Status.INFO, "Last name entered");
+					
+//					getElement(ObjectRepository.namesfx_txt).sendKeys("");
+//					test.log(Status.INFO, "Name suffix entered");
+					
+					getElement(ObjectRepository.strtaddrs_txt).sendKeys(strtaddress);
+					test.log(Status.INFO, "Street Address entered");
+					
+					getElement(ObjectRepository.zip_txt).sendKeys(zipcode);
+					test.log(Status.INFO, "Zip code entered");
+					
+					getElement(ObjectRepository.ssn_txt).sendKeys(ssn);
+					test.log(Status.INFO, "SSN entered");
+					
+					//dt
+					getElement(ObjectRepository.dob_cal).sendKeys(dob);
+					test.log(Status.INFO, "Date of Birth entered");
+					
+					selectDropdownOptContain(ObjectRepository.idtyp_drop, idtyp);
+					test.log(Status.INFO, "ID type selected");
+					
+					getElement(ObjectRepository.idnmbr_txt).sendKeys(idnumber);
+					test.log(Status.INFO, "ID number entered");
+					
+					getElement(ObjectRepository.issuedt_cal).sendKeys(issue_dt);
+					test.log(Status.INFO, "Issue date entered");
+					
+					getElement(ObjectRepository.expdt_cal).sendKeys(expr_dt);
+					test.log(Status.INFO, "Expiry date entered");
+					
+					selectDropdownOptContain(ObjectRepository.hrabttdecu_drop, hear_opt);
+					test.log(Status.INFO, "How did you hear about TDECU option selected");
+					
+					getElement(ObjectRepository.mothername_txt).sendKeys(mothername);
+					test.log(Status.INFO, "Mother name entered");
+					
+					getElement(ObjectRepository.crrntemplyr_txt).sendKeys(crrnt_emplyr);
+					test.log(Status.INFO, "Current Employer name entered");
+					
+					getElement(ObjectRepository.occptn_txt).sendKeys(occupation);
+					test.log(Status.INFO, "Occupation name entered");
+					
+					getElement(ObjectRepository.phonenmbr_txt).sendKeys(phonenmbr);
+					test.log(Status.INFO, "Phone number entered");
+					
+					selectDropdownOptContain(ObjectRepository.phonetyp_drop, phonetyp);
+					test.log(Status.INFO, "Phone type selected");
+					
+					getElement(ObjectRepository.primaryemail_txt).sendKeys(primary_email);
+					test.log(Status.INFO, "Primary email address entered");
+					
+					getElement(ObjectRepository.continue_further).click();
+					verifyElementPresent(ObjectRepository.memberShipEligibilityPage);
+					test.log(Status.INFO, "Navigated to Member Eligibility Page successfully");
+					getElement(ObjectRepository.backButton).click();
+					verifyText(ObjectRepository.primaryApplicantInfoPageTitle,"Primary Applicant Information");
+					
+					//Deleteing the value from phone number field and click on continue then check
+					getElement(ObjectRepository.phonenmbr_txt).clear();
+					test.log(Status.INFO, "Phone number deleted");
+					getElement(ObjectRepository.continue_further).click();
+					verifyText(ObjectRepository.errorMsg1,"Please Enter a Phone Number.");
+					if(getElement(ObjectRepository.phonenmbr_txt).getAttribute("style").contains("lightyellow")){
+						Assert.assertTrue(true, "Phone Number filed got highlighted with Yellow background");
+					}
+					
+					//Entering a valid value in Phone Number field and procced
+					getElement(ObjectRepository.phonenmbr_txt).sendKeys(phonenmbr);
+					test.log(Status.INFO, "Phone number entered");
+					getElement(ObjectRepository.continue_further).click();
+					verifyElementPresent(ObjectRepository.memberShipEligibilityPage);
+					test.log(Status.INFO, "Navigated to Member Eligibility Page successfully");
+					getElement(ObjectRepository.backButton).click();
+					verifyText(ObjectRepository.primaryApplicantInfoPageTitle,"Primary Applicant Information");
+					
+					//Keeping Phone Type field as Blank and check
+					selectDropdownOptContain(ObjectRepository.phonetyp_drop, "Phone Type");
+					test.log(Status.INFO, "Phone type selected");
+					getElement(ObjectRepository.continue_further).click();
+					verifyText(ObjectRepository.errorMsg1,"Please select a phone type.");
+					
+					//Enter valid Phone Type and procced
+					selectDropdownOptContain(ObjectRepository.phonetyp_drop, phonetyp);
+					test.log(Status.INFO, "Phone type selected");
+					getElement(ObjectRepository.continue_further).click();
+					verifyElementPresent(ObjectRepository.memberShipEligibilityPage);
+					test.log(Status.INFO, "Navigated to Member Eligibility Page successfully");
+					getElement(ObjectRepository.backButton).click();
+					verifyText(ObjectRepository.primaryApplicantInfoPageTitle,"Primary Applicant Information");
+					
+					//Deleting Email address from the field and check
+					getElement(ObjectRepository.primaryemail_txt).clear();
+					getElement(ObjectRepository.continue_further).click();
+					verifyText(ObjectRepository.errorMsg1,"The email address cannot be verified. Please check and retry.");
+					
+					
+					//Entering valid email address and check
+					getElement(ObjectRepository.primaryemail_txt).clear();
+					getElement(ObjectRepository.primaryemail_txt).sendKeys(primary_email);
+					getElement(ObjectRepository.continue_further).click();
+					verifyElementPresent(ObjectRepository.memberShipEligibilityPage);
+					test.log(Status.INFO, "Navigated to Member Eligibility Page successfully");
+					getElement(ObjectRepository.backButton).click();
+					verifyText(ObjectRepository.primaryApplicantInfoPageTitle,"Primary Applicant Information");
+					
+				 }
+
+				 }
+			 }
+		}
+				
+	
+
+	@AfterMethod
+	public void afterMethod(ITestResult result) throws Throwable {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			test.log(Status.FAIL, "Test Failed-" + result.getThrowable());
+
+			takescreenshot(this.getClass().getSimpleName(), test);
+		} else {
+			test.log(Status.PASS, "Verify that Non member user should get alert for invalid email address entry");
+		}
+	}
+
+	@AfterTest
+	public void tearDown() {
+		extent.flush();
+		driver.quit();
+	}
+
+}
