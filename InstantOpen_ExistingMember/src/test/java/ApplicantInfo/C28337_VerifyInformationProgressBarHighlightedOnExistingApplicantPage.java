3e55f2ae8e270a9a9032d0030019e25550e00dc3
Divya <divya.vagalaboina@eaglecrk.com>
2020-02-10 14:33:28 -0500

    Ädded new scripts for Progress bar validation as part of Existing member of InstantOpen

---
+++ InstantOpen_ExistingMember/src/test/java/ApplicantInfo/C28337_VerifyInformationProgressBarHighlightedOnExistingApplicantPage.java
@@ -0,0 +1,117 @@
+package ApplicantInfo;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.FrameworkComponents.BaseClass;
+import com.FrameworkComponents.ExtentManager;
+import com.FrameworkComponents.GenericKeywords;
+import com.FrameworkComponents.ObjectRepository;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.github.javafaker.Faker;
+
+public class C28337_VerifyInformationProgressBarHighlightedOnExistingApplicantPage extends GenericKeywords {
+
+	ExtentReports extent;
+	ExtentTest test;
+	/*
+	 * As an applicant, I expect the Information step to be highlighted when I am on the Existing Applicant page.
+	 */
+
+	@BeforeTest
+	public void setUp() throws InterruptedException, MessagingException, IOException {
+		
+		extent = ExtentManager.GetExtent();
+		test = extent.createTest(this.getClass().getSimpleName());
+		BaseClass iDriver = new BaseClass();
+		iDriver.myTestCaseName = this.getClass().getSimpleName();
+		executeDriverScript();
+		test.log(Status.INFO, "Application url navigated");
+		
+	}	
+	
+	@Test
+	public void C28337_VerifyInformationProgressBarHighlightedOnExistingApplicantPage() throws InterruptedException, MessagingException, IOException
+	{
+		
+		if(continuetestcase==true)
+		{
+			sheetName = "ProdData";
+			int totalRowCount = excl.getRowCount(sheetName);
+			for(startIter=1;startIter<=totalRowCount;startIter++) //It is mandatory to have this for loop in every test case
+			 {	
+				
+				//Start as a Member
+				scrollToElement(ObjectRepository.mmbrstrt_btn);
+				getElement(ObjectRepository.mmbrstrt_btn).click();
+				test.log(Status.INFO, "Members Start Here button clicked");
+				
+				
+				//select Classic Credit Card option
+				getElement(ObjectRepository.creditCardExpand).click();
+				getElement(ObjectRepository.platinumCheckBox).click();
+				test.log(Status.INFO, "Platinum credit card selected");
+				getElement(ObjectRepository.productPageNext).click();
+				test.log(Status.INFO, "Continue button clicked");
+				
+				getElement(ObjectRepository.prodLimitTextbox).sendKeys("100000");
+				getElement(ObjectRepository.prodInfoNextButton).click();
+				test.log(Status.INFO, "Credit card amount entered");
+				
+				getElement(ObjectRepository.mmbrVerifySSN).sendKeys("765437896");
+				getElement(ObjectRepository.mmbrVerifyDOB).sendKeys("02/01/1960");
+				getElement(ObjectRepository.mmbrVerifyNext).click();
+				test.log(Status.INFO, "Member Verified");
+				//Return all elements on progressbar
+				
+				int index=1;
+				for(WebElement eachOption :returnElements(ObjectRepository.progressBarOptions)){
+					
+					String text_from_option = eachOption.getText();
+					String class_prop = eachOption.findElement(By.xpath("(//span[@id='progBar']//span/..)["+index+"]")).getAttribute("class");
+					if(text_from_option.equalsIgnoreCase("Information") && class_prop.equals("JourneyBarCurrent")){
+						Assert.assertTrue(true, "Information icon is currently selected and highlighted on the page");
+						return;
+					}else if(text_from_option.equalsIgnoreCase("Products") && class_prop.equals("JourneyBarPrevious")){
+						Assert.assertTrue(true, "Products icon is currently selected and highlighted in different blue color");
+					}
+					index++;
+				}
+
+			 }
+		}
+				
+	}
+
+	@AfterMethod
+	public void afterMethod(ITestResult result) throws Throwable {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			test.log(Status.FAIL, "Test Failed-" + result.getThrowable());
+
+			takescreenshot(this.getClass().getSimpleName(), test);
+		} else {
+			test.log(Status.PASS, "As an applicant, I expect the Information step to be highlighted when I am on the Existing Applicant page.");
+		}
+	}
+
+	@AfterTest
+	public void tearDown() {
+		extent.flush();
+		driver.quit();
+	}
+
+}
